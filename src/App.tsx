465ad4e5687e4f8f137afa2642ff5bb38b54f56c
Letícia da Rosa <ley.darosa24@gmail.com>
2026-01-08 19:03:07 -0300

    feat: creating useful components

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,21 @@ import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
 import Badge from "./components/badge";
 import Alert from "./components/alert";
 import Divider from "./components/divider";
+import InputText from "./components/input-text";
+import SearchIcon from "./assets/icons/search.svg?react";
+import InputCheckbox from "./components/input-checkbox";
+import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
+import ImageFilePreview from "./components/image-file-preview";
+import { Dialog, DialogClose, DialogTrigger } from "@radix-ui/react-dialog";
+import { DialogBody, DialogContent, DialogFooter, DialogHeader } from "./components/dialog";
+import Text from "./components/text";
 
 export default function App() {
+	const form = useForm();
+	const file = form.watch("file");
+	const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
+
 	return (
 		<div className="grid gap-7 p-6">
 			<div className="flex gap-3">
@@ -47,6 +60,46 @@ export default function App() {
 
 			<div>
 				<Divider />
+			</div>
+
+			<div>
+				<InputText placeholder="Buscar foto" icon={SearchIcon} />
+			</div>
+
+			<div>
+				<InputCheckbox />
+			</div>
+
+			<div>
+				<InputSingleFile
+					form={form}
+					allowedExtensions={['png', 'jpg', 'jpeg', 'webp']}
+					maxFileSizeInMB={50}
+					replaceBy={<ImageFilePreview src={fileSource} alt="Preview" />}
+					{...form.register("file")}
+				/>
+			</div>
+
+			<div>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button>Abrir Modal</Button>
+					</DialogTrigger>
+
+					<DialogContent>
+						<DialogHeader>Teste dialog</DialogHeader>
+						<DialogBody>
+							<Text as="div" className="mb-4">Teste de conteudo do dialog</Text>
+						</DialogBody>
+						<DialogFooter>
+							<DialogClose asChild>
+								<Button variant="secondary">Cancelar</Button>
+							</DialogClose>
+
+							<Button>Adicionar</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
 			</div>
 		</div>
 	);
